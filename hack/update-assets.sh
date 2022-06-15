8ccfd9137f47a36d2b82cc79c07d54d3e82677d0
Christian Weichel <chris@gitpod.io>
2022-06-15 21:34:22 +0000

    Embed assets into binary

---
+++ hack/update-assets.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+tmpdir="$(mktemp -d)"
+bldname="assets$(date +%s)"
+base="$(dirname "$0")/.."
+
+SUPERVISOR="$(jq -r '.supervisor' "$base/pkg/builder/images.json")"
+WEBIDE="$(jq -r '."gitpod-code"' "$base/pkg/builder/images.json")"
+OPENVSCODE="$(jq -r '."open-vscode"' "$base/pkg/builder/images.json")"
+
+cat <<EOF > "$tmpdir/Dockerfile"
+FROM $SUPERVISOR AS supervisor
+FROM $WEBIDE AS webide
+FROM --platform=linux/amd64 $OPENVSCODE AS openvscode
+
+FROM alpine:3.16 AS staging
+
+RUN mkdir /staging
+COPY --from=supervisor /.supervisor /staging/supervisor/
+COPY --from=openvscode --chown=33333:33333 /home/.openvscode-server /staging/ide/
+COPY --from=webide --chown=33333:33333 /ide/startup.sh /ide/codehelper /staging/ide/
+COPY --from=webide --chown=33333:33333 /ide/extensions/gitpod-web /staging/ide/extensions/gitpod-web/
+RUN echo '{"entrypoint": "/ide/startup.sh", "entrypointArgs": [ "--port", "{IDEPORT}", "--host", "0.0.0.0", "--without-connection-token", "--server-data-dir", "/workspace/.vscode-remote" ]}' > /staging/ide/supervisor-ide-config.json && \
+    (echo '#!/bin/bash -li'; echo 'cd /ide || exit'; echo 'exec /ide/codehelper "\$@"') > /staging/ide/startup.sh && \
+    chmod +x /staging/ide/startup.sh && \
+    mv /staging/ide/bin/openvscode-server /staging/ide/bin/gitpod-code
+
+RUN cd /staging && tar cvvfz /assets.tar.gz .
+
+FROM alpine:3.16
+COPY --from=staging /assets.tar.gz /
+
+EOF
+
+docker build -t "$bldname" "$tmpdir"
+docker run --rm -i -v "$(realpath "$base/pkg/builder/assets"):/out" "$bldname" cp /assets.tar.gz /out/assets.tar.gz
--- pkg/builder/docker.go
+++ pkg/builder/docker.go
@@ -5,6 +5,7 @@
 package builder
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -14,6 +15,7 @@ import (
 	"strings"
 
 	gitpod "github.com/gitpod-io/gitpod/gitpod-protocol"
+	"github.com/gitpod-io/gitpod/run-gp/pkg/builder/assets"
 )
 
 type DockerBuilder struct {
@@ -28,47 +30,79 @@ func (db DockerBuilder) BuildImage(logs io.WriteCloser, ref string, cfg *gitpod.
 	}
 	defer os.RemoveAll(tmpdir)
 
-	df := `
-	FROM $SUPERVISOR AS supervisor
-	FROM $WEBIDE AS webide
-	FROM --platform=linux/amd64 $OPENVSCODE AS openvscode
+	var (
+		assetsHeader string
+		assetsCmds   string
+	)
+	if assets.IsEmbedded() {
+		err := assets.Extract(tmpdir)
+		if err != nil {
+			return err
+		}
+		assetsCmds = `
+		COPY supervisor/ /.supervisor/
+		COPY ide/ /ide/
+		`
+	} else {
+		assetsHeader = `
+		FROM $SUPERVISOR AS supervisor
+		FROM $WEBIDE AS webide
+		FROM --platform=linux/amd64 $OPENVSCODE AS openvscode
+		`
+		assetsHeader = strings.ReplaceAll(assetsHeader, "$SUPERVISOR", db.Images.Supervisor)
+		assetsHeader = strings.ReplaceAll(assetsHeader, "$WEBIDE", db.Images.WebIDE)
+		assetsHeader = strings.ReplaceAll(assetsHeader, "$OPENVSCODE", db.Images.OpenVSCode)
 
-	FROM $BASEIMAGE
-
-	COPY --from=supervisor /.supervisor /.supervisor/
-	COPY --from=openvscode --chown=33333:33333 /home/.openvscode-server /ide/
-	COPY --from=webide --chown=33333:33333 /ide/startup.sh /ide/codehelper /ide/
-	COPY --from=webide --chown=33333:33333 /ide/extensions/gitpod-web /ide/extensions/gitpod-web/
-	RUN echo '{"entrypoint": "/ide/startup.sh", "entrypointArgs": [ "--port", "{IDEPORT}", "--host", "0.0.0.0", "--without-connection-token", "--server-data-dir", "/workspace/.vscode-remote" ]}' > /ide/supervisor-ide-config.json && \
-		(echo '#!/bin/bash -li'; echo 'cd /ide || exit'; echo 'exec /ide/codehelper "$@"') > /ide/startup.sh && \
-		chmod +x /ide/startup.sh && \
-		mv /ide/bin/openvscode-server /ide/bin/gitpod-code
-
-
-	USER root
-	RUN rm /usr/bin/gp-vncsession || true
-	RUN mkdir -p /workspace && \
-		chown -R 33333:33333 /workspace
-	`
-	df = strings.ReplaceAll(df, "$SUPERVISOR", db.Images.Supervisor)
-	df = strings.ReplaceAll(df, "$WEBIDE", db.Images.WebIDE)
-	df = strings.ReplaceAll(df, "$OPENVSCODE", db.Images.OpenVSCode)
+		assetsCmds = `
+		COPY --from=supervisor /.supervisor /.supervisor/
+		COPY --from=openvscode --chown=33333:33333 /home/.openvscode-server /ide/
+		COPY --from=webide --chown=33333:33333 /ide/startup.sh /ide/codehelper /ide/
+		COPY --from=webide --chown=33333:33333 /ide/extensions/gitpod-web /ide/extensions/gitpod-web/
+		RUN echo '{"entrypoint": "/ide/startup.sh", "entrypointArgs": [ "--port", "{IDEPORT}", "--host", "0.0.0.0", "--without-connection-token", "--server-data-dir", "/workspace/.vscode-remote" ]}' > /ide/supervisor-ide-config.json && \
+			(echo '#!/bin/bash -li'; echo 'cd /ide || exit'; echo 'exec /ide/codehelper "$@"') > /ide/startup.sh && \
+			chmod +x /ide/startup.sh && \
+			mv /ide/bin/openvscode-server /ide/bin/gitpod-code
+		`
+	}
 
 	var baseimage string
 	switch img := cfg.Image.(type) {
 	case nil:
 		baseimage = "FROM gitpod/workspace-full:latest"
 	case string:
-		baseimage = img
-	case gitpod.Image_object:
-		fc, err := ioutil.ReadFile(filepath.Join(db.Workdir, img.Context, img.File))
+		baseimage = "FROM " + img
+	case map[string]interface{}:
+		fc, err := json.Marshal(img)
+		if err != nil {
+			return err
+		}
+		var obj gitpod.Image_object
+		err = json.Unmarshal(fc, &obj)
+		if err != nil {
+			return err
+		}
+		fc, err = ioutil.ReadFile(filepath.Join(db.Workdir, obj.Context, obj.File))
 		if err != nil {
 			// TODO(cw): make error actionable
 			return err
 		}
 		baseimage = "\n" + string(fc) + "\n"
+	default:
+		return fmt.Errorf("unsupported image: %v", img)
 	}
-	df = strings.ReplaceAll(df, "$BASEIMAGE", baseimage)
+
+	df := `
+	` + assetsHeader + `
+	` + baseimage + `
+	` + assetsCmds + `
+
+	USER root
+	RUN rm /usr/bin/gp-vncsession || true
+	RUN mkdir -p /workspace && \
+		chown -R 33333:33333 /workspace
+	`
+
+	fmt.Fprintf(logs, "\nDockerfile:%s\n", df)
 
 	err = ioutil.WriteFile(filepath.Join(tmpdir, "Dockerfile"), []byte(df), 0644)
 	if err != nil {
