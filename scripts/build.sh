4a07cad25cca1bdcffd5eac7a7c4a3679f2d8952
Victor Nogueira <victor@gitpod.io>
2022-12-22 14:35:00 +0000

    When running `publish` also run `build`

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,9 @@
 # Licensed under the GNU Affero General Public License (AGPL).
 # See License-AGPL.txt in the project root for license information.
 
+set -e
+
 ROOT="$(dirname "$(dirname "$(readlink -f "$0")")")"
 
-go build . -o run-gp
+go build -o run-gp .
 docker build -t gitpod/gp-run:latest $ROOT
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,5 +3,10 @@
 # Licensed under the GNU Affero General Public License (AGPL).
 # See License-AGPL.txt in the project root for license information.
 
+set -e
+
+ROOT="$(dirname "$(dirname "$(readlink -f "$0")")")"
+
+sh $ROOT/scripts/build.sh
 docker login --username "$DOCKER_HUB_USERNAME" --password "$DOCKER_HUB_PASSWORD"
 docker push gitpod/gp-run:latest
